f38af7299e109c03126c7a64d5bee478fc17350a
jawher <111719393+jawherbach@users.noreply.github.com>
2022-11-21 22:31:54 +0100

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{DashboardComponent}from './dashboard/dashboard.component'
+import {FormulaireComponent}from './formulaire/formulaire.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },   
+  { path: 'formulaire', component: FormulaireComponent },  
+
+  
+];
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmailValidatorDirective } from './email-validator.directive';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { MenuComponent } from './menu/menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+// import {MatMenuModule} from '@angular/material/menu';
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     EmailValidatorDirective,
+    FormulaireComponent,
+    MenuComponent,
+    DashboardComponent,
+
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
+  
+  
   ],
   providers: [
   ],
